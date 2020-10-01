5ecccb52fb3fa0571464cd2fbce91d16728cb8c5
ccranson27 <rcharl7@vt.edu>
2020-10-01 09:47:48 -0400

    Added in client.h and client.cpp as a basic framework

---
+++ client.cpp
@@ -0,0 +1,90 @@
+#include "client.h"
+#include <iostream>
+#include <cpprest/http_listener.h>
+
+#define JSTR json::value::string
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+//using namespace web::http::experimental::listener;
+/*Sources Used
+* https://mariusbancila.ro/blog/2017/11/19/revisited-full-fledged-client-server-example-with-c-rest-sdk-2-10/
+* https://mariusbancila.ro/blog/2013/08/19/full-fledged-client-server-example-with-cpprest-sdk-110/
+*/
+using std::cout;
+using std::cerr;
+using std::endl;
+
+
+/*!
+ * \brief Dispatches the request 
+ * \param clientt http_client used to send request
+ * \param mtd Used to determine if the method is safe or not (specifically if it is GET)
+ * \param jvalue The json value that can be sent to the console  
+ */
+pplx::task<http_response> make_task_request(http_client & client, 
+                                            method mtd, 
+                                            json::value const & jvalue)
+{
+   return (mtd == methods::GET || mtd == methods::POST) ? 
+      client.request(mtd) : 
+      client.request(mtd);
+}
+ 
+/*!
+ * \brief dispatches the request and when the response arrives it displays to std out
+ * \param client http_client used to send request
+ * \param mtd Used to determine if the method is safe or not (specifically if it is GET)
+ * \param jvalue The json value that can be sent to the console  
+ */ 
+void make_request(http_client & client, method mtd, json::value const & jvalue)
+{
+   make_task_request(client, mtd, jvalue)
+      .then([](http_response response)
+      {
+         if (response.status_code() == status_codes::OK)
+         {
+            return response.extract_json();
+         }
+         return pplx::task_from_result(json::value());
+      })
+      .wait();
+}
+
+
+Client::Client(const std::wstring key, const std::wstring value, const std::string command)
+{
+    http_client client(U("http://localhost")); //change this to address later 
+
+   
+    //NOTE: may have to change the value paramater into a double instead of just using 
+    // a string 
+
+    //Useful video https://www.youtube.com/watch?v=D7fiNQX7P5w
+ 
+   //json::value::field_map postvalue;
+   //stoped above this line
+   
+ 
+   //wcout << L"\nput values\n";
+   auto postvalue = json::value::array();
+   //postvalue[0] = json::value::string(key);
+   //postvalue[1] = json::value::string(value);
+   if (command == "POST")
+   {
+      //postvalue.push_back(std::make_pair(json::value(key), json::value(value)));
+      make_request(client, methods::POST,json::value::null()/*, json::value::object(postvalue)*/); //post value
+   }
+   else if (command == "GET")
+   {
+      make_request(client, methods::GET, json::value::null());// make get request
+      //auto getvalue = json::value::array(); //get each json value 
+   }   
+ 
+   //getvalue[0] = json::value(key);
+
+}
+
+Client::~Client() {
+}
--- client.h
+++ client.h
@@ -1,23 +1,33 @@
 #ifndef HOSPITALBEDS_CLIENT_H
 #define HOSPITALBEDS_CLIENT_H
 
+#include <algorithm>
 #include <string>
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
 #include "db_connection.h"
+#pragma comment(lib, "cpprest_2_10")
 
 /*!
- * The server manages outside connections and the internal database
+ * The client sends requests to retrieve and modify resources 
  */
 class Client {
 public:
     /*!
-     * Creates a new client at the specified address. Can only respond to GETs and POSTs
+     * Creates a new client at the specified address. Can only send GETs and POSTS
      * \param addr The URL at which to listen on
      */
-    explicit Server(const std::string & addr);
-    ~Server();
+    explicit Client(const std::wstring key, const std::wstring value, const std::string command);
+    ~Client();
 private:
     std::string address;
+
 };
 
+//void make_request(http_client & client, method mtd, json::value const & jvalue);
+//pplx::task<http_response> make_task_request(http_client & client, 
+                              //              method mtd, 
+                               //             json::value const & jvalue);
 
-#endif //HOSPITALBEDS_SERVER_H
+
+#endif //HOSPITALBEDS_CLIENT_H
