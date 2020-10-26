04207db171dc5c76683accea7d697d40c65b5b62
ccranson27 <rcharl7@vt.edu>
2020-10-26 06:49:06 -0700

    Added framework for adding a hospital to the dB

--- hospitalwindow.h
+++ hospitalwindow.h
@@ -9,6 +9,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class HospitalWindow; }
 QT_END_NAMESPACE
 
+const int handleLength = 7;
+
 /*!
  * \brief Displays data relevant to hospitals
  * The hospital window is the UI shown the hospital. It is where an administrator may see the filled and available beds,
@@ -24,9 +26,12 @@ public:
 private slots:
     void on_hospital_data_pressed();
 
+    void on_addHospital_pressed();
+
 private:
     Ui::HospitalWindow *ui;
-    //DBConnection * db;
+    int bedCount;
+    std::chrono::time_point<std::chrono::steady_clock> start;
     std::vector<Hospital> *hospitals;
     Client * hospitalClient;
     void generateBedData(); //TODO: This function will be generating the bed data
